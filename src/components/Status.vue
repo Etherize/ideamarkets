e15901c3c16cb3b665889f41505614ddd63fd3d4
jierdin <aitheric@protonmail.com>
2020-09-03 03:42:32 -0500

    Added Status Bar, sketched out other components further

---
+++ src/components/Status.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-container>
+    <b-row cols="3">
+      <b-col><button className="status_button"></button></b-col>
+      <b-col><button className="status_button"></button></b-col>
+      <b-col><button className="status_button"></button></b-col>
+    </b-row>
+   </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Status',
+}
+</script>
+
+<style scoped>
+.status_button {
+    min-width: 126px;
+    min-height: 54px;
+    border-radius: 15px;
+    background-color: red;
+  }
+</style>
