8e36171de55b9a65ba3b81de7e35a85532e957b2
Jonathan Kang <jon.m.kang@gmail.com>
2020-09-25 13:51:16 -0400

    completed styling on ethpay

--- src/components/EthPay.vue
+++ src/components/EthPay.vue
@@ -3,14 +3,31 @@
     <div  v-on:click='onBack' class='back_btn'>
         <img src='@/assets/back-arrow.png' class='back_btn_img'/>
     </div>
-    <p>Send ETH to this address</p>
+    <p
+    class="eth-title">Send ETH to this address</p>
     <b-container>
       <b-row cols="2">
-          <b-col>
-            <img src='@/assets/eth-address.jpg'/>
+          <b-col class="eth-qr-code">
+            <img class="eth-qr-img" src='@/assets/eth-address.jpg'/>
+            <b-row class="eth-stats">
+                <div class="eth">
+                  0.024
+                </div>
+                <div class="type-eth">
+                ETH
+                </div>
+            </b-row>
+            <b-row class="eth-stats">
+                <div class="dollars">
+                $20.00
+                </div>
+                <div class="type-dollars">
+                USD
+                </div>
+            </b-row>
           </b-col>
-          <b-col>
-            0x12321312312312f
+          <b-col class="loading-numbers">
+            0x12321312312312fADFSDFEF12dfADF
           </b-col>
       </b-row>
     </b-container>
@@ -34,5 +51,78 @@ export default {
 </script>
 
 <style scoped>
+.eth-title{
+  margin-top: 2rem;
+  color: white; 
+  text-align: left;
+}
 
+.eth-qr-code{
+  width: 280px;
+  height: 200px;
+  padding: 1rem;
+  background:white;
+  border-radius: 18px;
+}
+
+.eth-qr-img{
+  margin: auto;
+}
+
+.eth-stats{
+  justify-content: center;
+}
+
+.eth, .dollars{
+  padding: 3px;
+  text-align: right;
+}
+
+.eth{
+  font-weight: bold;
+  font-size: 14pt;
+}
+
+
+
+.type-eth, .type-dollars{
+  display: inline-block;
+  margin: 4px;
+  text-align: left;
+}
+
+.type-eth{
+  display: inline-block;
+  border-radius: 8px;
+  padding: 3px;
+  color: white;
+  font-weight: bold;
+  font-size: 10pt;
+  background: rgba(0,0,0,.3);
+}
+
+.dollars{
+  padding: 0;
+  padding-right: 3px;
+  font-size: 10pt;
+  font-weight: bold;
+}
+
+.type-dollars{
+  color: rgba(0,0,0,.3);
+  margin: 0;
+  padding: 0;
+  padding-left: 5px;
+  font-size: 10pt;
+  font-weight: 900;
+}
+
+.loading-numbers{
+  font-weight: 600;
+  color: white;
+  padding: 2rem;
+  font-size: 14pt;
+  word-wrap: break-word;
+  text-align:left;
+}
 </style>
